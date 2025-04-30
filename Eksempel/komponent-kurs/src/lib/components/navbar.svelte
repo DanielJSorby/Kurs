97872c83710da5e10935774e1711e4497bbca1b7
Daniel Johan Sørby <81819141+DanielJSorby@users.noreply.github.com>
2025-04-30 09:56:48 +0200

    Norias greier ass

---
+++ Eksempel/komponent-kurs/src/lib/components/navbar.svelte
@@ -0,0 +1,33 @@
+<nav>
+    <ul>
+        <li><a href="/">Hjem</a></li>
+        <li><a href="/blog">Blog</a></li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        background-color: #25262b;
+        padding: 1rem 2rem;
+        border-radius: 26px;
+    }
+
+    ul {
+        list-style: none;
+        display: flex;
+        gap: 2rem;
+    }
+
+    a {
+        text-decoration: none;
+        color: #FDFFFF;
+        font-family: var(--font-heading);
+        font-weight: 500;
+        font-size: 1rem;
+        transition: color 0.2s ease;
+    }
+
+    a:hover {
+        color: #FF3E00;
+    }
+</style>
